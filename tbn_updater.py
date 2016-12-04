55ea333cb0dd6b9a324c1c1214ba5245ab347971
amazingr4b <amazingr4b@gmail.com>
2016-12-04 13:33:08 -0600

    testing update script
    used for automatic updates. can be launched manually to easly update as well. 
    NOTE: This is a test version. Backup your files before using.

---
+++ tbn_updater.py
@@ -0,0 +1,294 @@
+import os
+import sys
+import getpass
+import subprocess
+import sqlite3
+import urllib3
+import platform
+
+#urllib3.disable_warning()
+
+def worklist(thearray):
+	if int(len(thearray) == 0):
+		return ("Error: No results found.")
+	movies = thearray
+	mcount = 1
+	mvcount = 0
+	mmin = 0
+	mmax = 9
+	mpmin = 1
+	if mmax >= len(movies):
+		mmax = int(len(movies))-1
+	exitc = ""
+	while "quit" not in exitc:
+		cls()
+		try:
+			print ("Error: " + Error + "\nThe Following Items Were Found:\n")
+			del Error
+		except NameError:
+			print ("The Following Items Were Found:\n")
+		while mmin <= mmax:
+			print (str(mmin+1) + ": " + movies[mmin])
+			mmin = mmin + 1
+		print ("\nShowing Items " + str(mpmin) + " out of " + str(mmax+1)+ " Total Found: " + str(len(movies)))
+		print ("\nSelect an item to return the corresponding item.\nTo see a description of an item enter \'desc number\', where number is the corresponding number.\nTo jump to a letter enter \'letter a\', where a is the desired letter.\nEnter \'Yes\' to go to the next page, and \'No\' to exit.\n")
+		getme = input('Choice:')
+		if (("yes" in getme.lower()) and ("letter" not in getme.lower())):
+			mvcount = mvcount + 10
+			mpmin = mmax + 1
+			mmax = mmax + 10
+			if (mmax > int(len(movies)-1)):
+				mcheck = int(mmax) - int(len(movies)-1)
+				if ((mcheck > 0) and (mcheck < 10)):
+					mmax = mmax-mcheck
+				elif mcheck > 10:
+					return ("Done.")
+			if (mmax == int(len(movies)+9)):
+				return ("Done")
+		elif (("no" in getme.lower()) and ("letter" not in getme.lower())):
+			exitc = "quit"
+			return ("done")
+		else:
+			try:
+				return movies[int(getme)-1]
+			except Exception:
+				Error = "Invalid Selection. Please Try Again."
+				mmin = mmin - 10
+
+http = urllib3.PoolManager()
+
+try:
+	input = raw_input
+except NameError:
+	pass
+
+def cls():
+   os.system('cls' if os.name=='nt' else 'clear')
+
+ostype = platform.system()
+
+try:
+	extraopt = str(sys.argv[1])
+except Exception:
+	extraopt = "none"
+
+if "Linux" in ostype:
+
+	user = getpass.getuser()
+
+	if ("root" in user):
+		print ("Run this command as your non-root user, without sudo. You will be prompted for sudo access where it is necessary in this setup.")
+	else:
+
+		newdir = "/home/" + user + "/"
+		homedir = "/home/" + user + "/hasystem/"
+		writeme = "homedir = \'" + homedir + "\'\n"
+		writemehome = writeme
+
+		print ("Hello " + user + ". I am now adding the alias add script now. You may be prompted for sudo.\n")
+		
+		#updateing add_to_bash.py
+		if not os.path.exists(homedir):
+			os.makedirs(homedir)
+			sayme = "The necessary directory has been created.\n"
+		else:
+			sayme = "Necsessary Directory Exists. Moving on.\n"
+
+		print (sayme)
+		file0 = homedir + "add_to_bash.py"
+		cmd = "rm " + file0
+		try:
+			os.system(cmd)
+		except Exception:
+			pass
+		print ("Successfully removed add_to_bash.py. Getting latest file.")
+		try:
+			url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/add_to_bash.py"
+			newfile = http.request('GET', url, preload_content=False)
+			newfile = writemehome + str(newfile.data)
+			#print newfile
+			with open(file0, 'wb') as file:
+				file.write(newfile)
+			file.close()
+			print ("File successfully moved to the necessary directory.")
+		except IOError:
+			print ("warning add_to_bash.py does not exist. Alias commands will not work.")
+		
+		#get alias file
+		file10 = homedir + "aliases"
+        cmd = "rm " + file10
+		try:
+			os.system(cmd)
+		except Exception:
+			pass
+		print ("Successfully removed aliases. Getting latest file.")
+		try:
+			url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/aliases"
+			newfile = http.request('GET', url, preload_content=False)
+			#print (newfile)
+			with open(file10, 'wb') as file:
+				file.write(newfile.data)
+			file.close()
+			print ("File successfully moved to the necessary directory.")
+		except IOError:
+				print ("warning aliases does not exist. Alias commands will not work.")
+		try:
+			command = "sudo python " + file0 + " " + user.strip()
+			#print (command)
+			os.system(command)
+		except Exception:
+			print ("Failed to add aliases to Bash. see add_to_bash.py for list of alias it is recommended you add.\n")
+			updatecheck = "fail"
+
+
+		print ("Trying to add a cron entry for the play checking script now.\n")
+
+		#get add_to_cron.py
+		file00 = homedir + "add_to_cron.py"
+        cmd = "rm " + file00
+		try:
+			os.system(cmd)
+		except Exception:
+			pass
+		print ("Successfully removed add_to_cron.py. Getting latest file.")
+		try:
+			url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/add_to_cron.py"
+			newfile = http.request('GET', url, preload_content=False)
+			newfile = writemehome + str(newfile.data)
+			with open(file00, 'wb') as file:
+				file.write(newfile)
+			file.close()
+			print ("File successfully moved to the necessary directory.")
+		except IOError:
+			print ("warning add_to_cron.py does not exist. Cron add will fail.")
+		try:
+			command = "sudo python " + file00 + " " + user.strip()
+			os.system(command)
+		except Exception:
+			print ("Failed to add cronjob. see add_to_cron.py for recommended job.\n")
+			updatecheck = "fail"
+
+		
+
+else:
+	print ("Unable to add cron or bash entries on a Windows system. I am skipping those steps.")
+	
+	hcheck = "negative"
+	homedir = os.path.dirname(os.path.abspath(__file__))
+	#homedir = homedir + "hasystem\\"
+	writeme = "homedir = \'" + homedir + "\\'\n"
+	writemehome = writeme
+	
+
+	
+if not os.path.exists(homedir):
+	os.makedirs(homedir)
+	print (homedir + " has been successfully created.\n")
+else:
+	print (homedir + " already exists. Moving on.")
+	
+
+file5 = homedir + "system.py"
+try:
+	os.remove(file5)
+	print (Successfully removed system.py.")
+except Exception:
+	pass
+
+try:
+	url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/system.py"
+	newfile = http.request('GET', url, preload_content=False)
+	newfile = newfile.data
+	#print (newfile)
+	with open(file5, 'wb') as file:
+		file.write(newfile)
+	file.close()
+	with open (file5, 'r') as file:
+		rewrite = file.read()
+	file.close()
+	rewrite = writemehome + "\n" + rewrite
+	with open(file5, "w") as file:
+		file.write(rewrite)
+	file.close()
+	print ("system.py file successfully updated.")
+except FileNotFoundError:
+	print ("warning system.py does not exist. The TBN controller will not work.")
+file6 = homedir + "piplaystate.py"
+try:
+	os.remove(file6)
+	print ("Removed piplaystate.py. Getting newest version.")
+except Exception:
+	pass
+
+try:
+	url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/piplaystate.py"
+	newfile = http.request('GET', url, preload_content=False)
+	newfile = newfile.data
+	with open(file6, 'wb') as file:
+		file.write(newfile)
+	file.close()
+	with open (file6, 'r') as file:
+		rewrite = file.read()
+	file.close()
+	rewrite = writemehome + "\n" + rewrite
+	with open(file6, "w") as file:
+		file.write(rewrite)
+	file.close()
+	print ("piplaystate.py successfully updated.")
+except Exception:
+	print ("warning piplaystate.py does not exist. The play check status script will not work.")
+file7 = homedir + "playstatus.py"
+try:
+	os.remove(file7)
+	print ("Removed playstatus.py. Updating now.")
+except Exception:
+	pass
+	
+try:
+	url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/playstatus.py"
+	newfile = http.request('GET', url, preload_content=False)
+	newfile = newfile.data
+	with open(file7, 'wb') as file:
+		file.write(newfile)
+	file.close()
+	with open (file7, 'r') as file:
+		rewrite = file.read()
+	file.close()
+	rewrite = writeme + rewrite
+	with open(file7, "w") as file:
+		file.write(rewrite)
+	file.close()
+	print ("File successfully moved to the necessary directory.")
+except Exception:
+	print ("warning playstatus.py does not exist. The play check status script will not work.")
+	
+file8 = homedir + "upddatedb_pi.py"
+try:
+	os.remove(file8)
+	print ("Removed upddatedb_pi.py. Updating now.")
+	
+try:
+	url = "https://raw.githubusercontent.com/amazingr4b/TBN-Plex/master/upddatedb_pi.py"
+	newfile = http.request('GET', url, preload_content=False)
+	newfile = newfile.data
+	with open(file8, "wb") as file:
+		file.write(newfile)
+	file.close()
+	with open (file8, 'r') as file:
+		rewrite = file.read()
+	file.close()
+	rewrite = writeme + rewrite
+	with open(file8, "w") as file:
+		file.write(rewrite)
+	file.close()
+	
+	print ("File successfully moved to the necessary directory.")
+	updatecheck = "pass"
+except FileNotFoundError:
+	print ("warning updateddb_pi.py does not exist. The system will be unable to build the shows and movie tables in your database.")
+	updatecheck = "fail"
+		
+
+print ("Update Finished.\n")
+
+
